8fa9d6296bbe1436df22618f96764f9ced676f83
Your Nameksubondar <xeniabondar@yandex.ru>
2021-05-28 16:29:50 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
     var mixer = mixitup('.products__inner-box');
 
+    $('.rate-star').rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: '12px'
+    });
     
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    });
 });
